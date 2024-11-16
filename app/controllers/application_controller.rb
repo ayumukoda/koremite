f5d41b6aa977d76c41e2cd7903286a2055d4c573
ayumukoda <168876414+ayumukoda@users.noreply.github.com>
2024-11-16 03:34:16 +0000

    管理者機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,12 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!, except: [:top]
+    before_action :authenticate_user!, except: [:top], unless: :admin_controller?
     #ログインやユーザ登録の前にconfigure_permitted_parameter（パラメーターを取得）が使われる
-    before_action :configure_permitted_parameters, if: :devise_controller?
     
-    def after_sign_in_path_for(resources)
-        post_images_path
+    private
+ 
+    def admin_controller?
+        self.class.module_parent_name == 'Admin'
     end
+        
     
-    def after_sign_out_path_for(resources)
-        about_path
-    end
-    
-    protected
-    
-    #devise_parameter_sanitizer.permitメソッドを使うことでユーザー登録(sign_up)の際に、ユーザー名(name)のデータ操作を許可しています
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    end
 end
-#comfirm
